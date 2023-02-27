812ea817840dfc6359ffcf8f6fa7733249076b81
ChangMin <sopg04@gmail.com>
2023-02-28 00:01:17 +0900

    feat: 최근 검색어 저장 시 중복제거 및 최대 갯수 이상이면 오래된 검색어 제거

--- BookSearch/Data/CoreData/BookQueryStorage/CoreDataBookQueryStorage.swift
+++ BookSearch/Data/CoreData/BookQueryStorage/CoreDataBookQueryStorage.swift
@@ -24,7 +24,9 @@ extension CoreDataBookQueryStorage {
         coreDataStorage.performBackgroundTask { context in
             do {
                 let request = BookQueryEntity.fetchRequest()
-                request.sortDescriptors = [NSSortDescriptor(key: #keyPath(BookQueryEntity.createdAt), ascending: false)]
+                request.sortDescriptors = [
+                    NSSortDescriptor(key: #keyPath(BookQueryEntity.createdAt), ascending: false)
+                ]
                 request.fetchLimit = 10
                 let result = try context.fetch(request).map { $0.toDomain() }
                 
@@ -41,6 +43,8 @@ extension CoreDataBookQueryStorage {
     ) {
         coreDataStorage.performBackgroundTask { context in
             do {
+                try self.removeDuplicate(query: query, context: context)
+                try self.removeOldestQuery(query: query, context: context)
                 let entity = BookQueryEntity(query: query, insertInto: context)
                 try context.save()
                 
@@ -49,5 +53,29 @@ extension CoreDataBookQueryStorage {
                 completion(.failure(Error.self as! Error))
             }
         }
+    }
+    
+    /// 중복 query 제거
+    private func removeDuplicate(query: BookQuery, context: NSManagedObjectContext) throws {
+        let request: NSFetchRequest = BookQueryEntity.fetchRequest()
+        var result = try context.fetch(request)
+        
+        result.filter { $0.query == query.query }
+            .forEach { context.delete($0) }
+        result.removeAll { $0.query == query.query }
+    }
+    
+    /// 최대 10개 저장해서 오래된 query 제거
+    private func removeOldestQuery(query: BookQuery, context: NSManagedObjectContext) throws {
+        let request: NSFetchRequest = BookQueryEntity.fetchRequest()
+        request.sortDescriptors = [
+            NSSortDescriptor(key: "createdAt", ascending: false)
+        ]
+        let result = try context.fetch(request)
+
+        guard result.count > 9 else { return }
+        result.suffix(result.count - 9)
+            .forEach { context.delete($0) }
+
     }
 }
