b6ccddec6263d32569f77b74b72fbe746a586484
ChangMin <sopg04@gmail.com>
2023-12-15 18:05:56 +0900

    feat: Observable 반환하는 네트워크 메서드 구현

--- BookSearch/Infrastructure/Network/NetworkProvider.swift
+++ BookSearch/Infrastructure/Network/NetworkProvider.swift
@@ -7,12 +7,17 @@
 
 import Foundation
 
+import RxSwift
+
 protocol NetworkProviderProtocol {
     func request<R: Decodable, E: RequesteResponsable>(
         with endpoint: E,
         completion: @escaping (Result<R, Error>) -> Void
     ) where E.Response == R
 
+    func request<R: Decodable, E: RequesteResponsable>(
+        with endpoint: E
+    ) -> Observable<R> where E.Response == R
 }
 
 final class NetworkProvider: NetworkProviderProtocol {
@@ -63,6 +68,55 @@ final class NetworkProvider: NetworkProviderProtocol {
             
         } catch {
             completion(.failure(NetworkError.needErrorControl))
+        }
+    }
+    
+    func request<R: Decodable, E: RequesteResponsable>(
+        with endpoint: E
+    ) -> Observable<R> where E.Response == R {
+        return Observable<R>.create { observer in
+            do {
+                let urlRequest = try endpoint.getURLRequest()
+                
+                let task = self.session.dataTask(with: urlRequest) { data, response, error in
+                    if let error = error {
+                        observer.onError(error)
+                        return
+                    }
+                    
+                    guard let response = response as? HTTPURLResponse else {
+                        observer.onError(NetworkError.needErrorControl)
+                        return
+                    }
+                    
+                    guard (200...299).contains(response.statusCode) else {
+                        observer.onError(NetworkError.needErrorControl)
+                        return
+                    }
+                    
+                    guard let data = data else {
+                        observer.onError(NetworkError.needErrorControl)
+                        return
+                    }
+                    
+                    do {
+                        let decodeData = try JSONDecoder().decode(R.self, from: data)
+                        observer.onNext(decodeData)
+                        
+                    } catch {
+                        observer.onError(NetworkError.needErrorControl)
+                    }
+                }
+                
+                task.resume()
+                
+                return Disposables.create {
+                    task.cancel()
+                }
+                
+            } catch {
+                observer.onError(NetworkError.needErrorControl)
+            }
         }
     }
     
